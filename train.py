dae449f31babf6a6a895b9ed5f9ebb8e088e09b6
Nikita Pavlichenko <pilot7747@gmail.com>
2018-03-24 13:18:02 +0300

    6

--- train.py
+++ train.py
@@ -93,6 +93,7 @@ if __name__ == '__main__':
 
     if inputPath != "":
         filelist = glob.glob(inputPath + "*.txt")
+        print(filelist)
         for filename in filelist:
             f = open(filename)
             currentposition = 0
