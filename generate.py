dcf378628b746903c114251e71bff0506eee70d6
Nikita Pavlichenko <pilot7747@gmail.com>
2018-04-10 22:43:58 +0300

    123123

--- generate.py
+++ generate.py
@@ -3,17 +3,23 @@ import argparse
 import sqlite3
 import random
 
+# Берем единственное число из результата запроса
+
 
 def get_num(sql_cursor, query):
     sql_cursor.execute(query)
     row = [item[0] for item in sql_cursor.fetchall()]
     return row[0]
 
+# Достаем слово из модели
+
 
 def get_word(sql_cursor, query, list_arg):
     sql_cursor.execute(query, list_arg)
     row = [item[0] for item in sql_cursor.fetchall()]
     return row[0]
+
+# Находим следующее слово цепочки
 
 
 def get_next_word(sql_cursor, current_word):
@@ -28,21 +34,21 @@ def get_next_word(sql_cursor, current_word):
 
 
 if __name__ == '__main__':
+    # Парсим аргументы
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, help='path to '
-                                                  'model file', required=True)
-    parser.add_argument('--seed', type=str,
+    parser.add_argument('--model', action='store', type=str,
+                        help='path to model file', required=True)
+    parser.add_argument('--seed', action='store', type=str,
                         help='first word, if not defined - random')
-    parser.add_argument('--length', type=int,
+    parser.add_argument('--length', action='store', type=int,
                         required=True, help='number of printed words')
-    parser.add_argument('--output',
+    parser.add_argument('--output', action='store',
                         help='path to output file, '
                              'if not defined text will be printed to console')
     args = parser.parse_args()
     connectionStr = args.model
     conn = sqlite3.connect(connectionStr)
     cursor = conn.cursor()
-    size = get_num(cursor, "SELECT count(id) FROM t")
     word = ""
     random.seed()
     if args.seed:
@@ -53,12 +59,14 @@ if __name__ == '__main__':
         if seedExists == 0:
             raise ValueError('Seed word does not exist')
     else:
+        size = get_num(cursor, "SELECT count(id) FROM t")
         word = get_word(cursor,
                         "SELECT first FROM t WHERE id=?",
                         (random.randint(1, size), ))
     result = word
     currentLength = 1
     end = False
+    # Генерируем цепочку
     while currentLength != args.length and (not end):
         word = get_next_word(cursor, (word, ))
         if not word:
--- train.py
+++ train.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -*-
 import sqlite3
 import re
 from sqlite3 import Error
 import argparse
 import os
 import glob
+
+# Класс, чтобы менять цвет шрифта в консоли
 
 
 class BColors:
@@ -17,6 +18,8 @@ class BColors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# Создаем файл модели
+
 
 def create_connection(db_file):
     try:
@@ -26,7 +29,10 @@ def create_connection(db_file):
     finally:
         connection.close()
 
+
 lastWord = ""
+model = ()
+# Добавляем слово в модель
 
 
 def addword(conn, cursor, word1, word2):
@@ -39,6 +45,7 @@ def addword(conn, cursor, word1, word2):
     else:
         squerry = "UPDATE t SET num = num + 1 WHERE first = ? AND second = ?"
         cursor.execute(squerry, (word1, word2))
+# Парсим строчку и добавляем ее по словам в модель
 
 
 def add_row(connection, sql_cursor, text_line, to_lower):
@@ -57,26 +64,30 @@ def add_row(connection, sql_cursor, text_line, to_lower):
     if len(words) != 0:
         lastWord = words[-1]
 
+
 if __name__ == '__main__':
+    # Парсим аргументы
     parser = argparse.ArgumentParser()
     parser.add_argument('--input-dir', action='store',
                         type=str, help='path to folder with input files')
-    parser.add_argument('--model', type=str, help='path to model file')
+    parser.add_argument('--model', action='store',
+                        type=str, help='path to model file')
     parser.add_argument('--lc', action='store_true', help='to lower case')
-    parser.add_argument('--file', type=str, help='path to input file')
+    parser.add_argument('--file', action='store',
+                        type=str, help='path to input file')
     args = parser.parse_args()
-    current = 0
-    toLower = args.lc
-    inputPath = ""
-    connectionStr = "model.sqlite"
     inputPath = args.input_dir
+    connectionStr = "model.sqlite"
+    # Если пользователь не указал модель
     if args.model:
         connectionStr = args.model
     try:
+        # Если такой файл уже есть, то удаляем
         os.remove(connectionStr)
         create_connection(connectionStr)
     except OSError:
         pass
+    # Коннектимся к бд
     conn = sqlite3.connect(connectionStr)
     conn.execute("CREATE TABLE t(id INTEGER PRIMARY KEY AUTOINCREMENT "
                  "NOT NULL, first TEXT, second TEXT, num INTEGER)")
@@ -84,17 +95,21 @@ if __name__ == '__main__':
     conn.execute("CREATE INDEX first_word_index ON t(first, second)")
     conn.commit()
     cursor = conn.cursor()
-
+    toLower = args.lc
     if inputPath != "":
         file_list = list()
+        # Вдруг указан конкретный файл
         if args.file:
+            inputPath = ""
             file_list.append(inputPath + args.file)
         else:
+            # Добавляем все txt в папках в список
             for top, dirs, files in os.walk(args.input_dir):
                 for directory in dirs:
                     path = str(os.path.join(top, directory))
                     file_list += glob.glob(path + "/*.txt")
             file_list += glob.glob(args.input_dir + "/*.txt")
+        # Непосредственно добавляем все построчно в модель
         for filename in file_list:
             f = open(filename)
             current_position = 0
@@ -102,6 +117,7 @@ if __name__ == '__main__':
                 add_row(conn, cursor, line, toLower)
                 current_position += 1
     else:
+        # Чтение с консоли
         while True:
             line = input()
             if not line:
